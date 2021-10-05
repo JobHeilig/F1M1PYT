4a25a094c83b61c5f4d3a841348c8cae75f54bef
JobHeilig <90682561+JobHeilig@users.noreply.github.com>
2021-10-05 10:16:50 +0200

    Delete PYTB1L2PoemPrinting directory

--- PYTB1L2PoemPrinting/Poem.py
+++
@@ -1,6 +0,0 @@
-vers1 = "Al als een kleine jongen zat ik al veel te dromen om als succesvolle man op een game maker te worden. Maar toen ging ik verhuizen dat was nog even huilen maar do grooste schrok zat vreemd genoeg op school te schuilen."
-vers2 = "Ik heb toch autisme ik ben een beetje traag maar dat we geen excuus ik stond gewoon te laag. Opgeven zijden zij. Je heb autisme, kan jij niet aan. Ik heb nog steeds een haat voor de mensen die daar staan."
-vers3 = "En nu zit ik hier op mijn weg naar mijn droom. Het is lastig en een beetje zwaar maar dat zie je ook. Niet zeiken zeuren zolang je maar went. Maar dat is niets nieuws als jij Job Heilig bent."
-
-
-print(vers1,vers2, vers3)
