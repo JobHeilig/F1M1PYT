ac34250bc83bc59cf754619b941049ae92d9cce3
JobHeilig <30780@ma-web.nl>
2021-10-28 10:00:37 +0200

    Added PYTB1L4WheresWaldo

---
+++ PYTB1L4WheresWaldo/PYTB1L4WheresWaldo.py
@@ -0,0 +1,4 @@
+import random
+people = ["Waldo", "William", "Wesley", "Wyatt", "Walson", "Walter", "Waylon", "Winston", "Wallace", "Willie", "Waylen", "Wolf", "Wendel", "Woodrow", "Willow", "Willem", "Wayland", "Whitley", "Wolfe", "Williams", "Woods", "Welles", "Whoot", "Wilbur", "Wilfredo", "Whitten", "Wilfred", "Wario", "Warrick", "Winner", "Wright", "Wayden"]
+random.shuffle(people)
+print(people)
